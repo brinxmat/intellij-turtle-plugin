916e79e1a08205cd06b2ac507af5d97ec96d3be3
GitHub Action <action@github.com>
2021-09-04 15:46:46 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/brinxmat/intellijturtleplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.brinxmat.intellijturtleplugin.services
+
+import com.github.brinxmat.intellijturtleplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/brinxmat/intellijturtleplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/brinxmat/intellijturtleplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.brinxmat.intellijturtleplugin.services
 
+import com.github.brinxmat.intellijturtleplugin.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
